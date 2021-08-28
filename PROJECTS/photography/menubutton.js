30cc7c826893f0e643c2bc3fafb3962cbfca46db
Abinaya <88851407+AbinayaVijaykarnan@users.noreply.github.com>
2021-08-28 17:09:23 +0530

    Add files via upload

---
+++ PROJECTS/photography/menubutton.js
@@ -0,0 +1,7 @@
+
+let button=document.querySelector(".nav-arrow") ,
+    links=document.querySelector(".links") ;
+
+    button=addEventListener("click",()=>{
+      links.classList.toggle("display");
+    })
